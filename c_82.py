b428d30a64ed3971b3d1b8a8d1bd5b77e20d4946
AnuragV314 <vermagup01@gmail.com>
2022-07-29 14:08:43 +0530

    82

---
+++ c_82.py
@@ -0,0 +1,26 @@
+# --------------------------------------------------------
+#      <===: PYTHON DATABASE CONNECTIVITY (PDBC) :===>
+# --------------------------------------------------------
+
+
+# 1. Temporary storage:
+# ---------------------
+# d={}
+# name = input('Enter name: ')
+# addr = input('Enter address: ')
+# d[name]=addr
+# print(d)
+
+
+# 2. Permanent storage:
+# ---------------------
+# eg: files, database, data ware houses
+
+
+
+
+# Python Database Programming:=>
+# ------------------------------
+
+# NOTES :)
+
